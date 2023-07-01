a43d5231cb35fb7f4a1bad75e2e2cb24db4d0ccd
Alessandro Jin <a.jin@reply.it>
2023-07-02 00:03:10 +0200

    Changed name project

--- src/main/java/com/aixel/recipeme/CineBooking.java
+++ src/main/java/com/aixel/recipeme/CineBooking.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecipeMeApplication {
+public class CineBooking {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RecipeMeApplication.class, args);
+		SpringApplication.run(CineBooking.class, args);
 	}
 
 }
