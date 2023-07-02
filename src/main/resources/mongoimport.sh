aa6ac7134bdd8677ad2d52bb8299dfe6cbda6018
Alessandro Jin <a.jin@reply.it>
2023-07-02 18:53:18 +0200

    Added script to import users

---
+++ src/main/resources/mongoimport.sh
@@ -0,0 +1,6 @@
+mongoimport --uri 'mongodb+srv://largescale.o4ancwl.mongodb.net/LargeScale?retryWrites=true&w=majority' \
+   --username='largeScale' \
+   --password='welcome1' \
+   --collection='userDocument' \
+   --file='users.json' \
+   --jsonArray
